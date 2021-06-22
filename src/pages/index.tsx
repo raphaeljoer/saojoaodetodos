5eccff45b37ebfdf0293e0e779d053cce3befb26
raphaeljoer <ra.joer@gmail.com>
2021-06-22 01:22:51 -0300

    Winner - Added Winner on Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,15 @@
 //core components
 import Layout from '@/components/molecules/Layout';
 import Container from '@/components/molecules/Container';
-import Closed from '@/components/molecules/Closed';
-import Poll from '@/components/molecules/Poll';
+import Winner from '@/components/molecules/Winner';
 //resources
-import React, { useEffect, useState } from 'react';
-import { isAfter } from 'date-fns';
+import React from 'react';
+import { GetStaticProps } from 'next';
 import { NextSeo } from 'next-seo';
 //config
 import SEO from '@/config/seo';
 import { next } from '@/config/app';
-//next
-import { GetStaticProps } from 'next';
 //data
-import artists from '@/data/static/artists';
 import { getResults } from '@/data/request/results';
 //types
 import { ResultProps } from '@/@types/result';
@@ -23,21 +19,11 @@ interface HomePageProps {
 }
 
 export default function HomePage({ results }: HomePageProps) {
-  const [open, setOpen] = useState(false);
-
-  const limitDate = new Date(2021, 5, 18, 23, 59, 59);
-  const isAfterLimitDate = () => isAfter(new Date(), limitDate);
-
-  useEffect(() => {
-    isAfterLimitDate() ? setOpen(false) : setOpen(true);
-    setInterval(() => isAfterLimitDate() && setOpen(false), 1000);
-  }, []);
-
   return (
     <Layout>
       <NextSeo {...SEO.page.home} />
       <Container mt={16} mb={24}>
-        {open ? <Poll artists={artists} results={results} /> : <Closed />}
+        <Winner results={results} />
       </Container>
     </Layout>
   );
